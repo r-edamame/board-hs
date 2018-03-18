69d5d010a8f709a9bc62f6390b73d0abd4dc1d96
edamame <edamame15.r@gmail.com>
2018-03-18 18:28:22 +0900

    fix: add migration command

--- app/app/Main.hs
+++ app/app/Main.hs
@@ -6,11 +6,16 @@ module Main (main) where
 import Control.Monad.Logger (NoLoggingT(..))
 
 import Network.Wai.Handler.Warp (run, Port)
+import Database.Persist.Sql (runSqlPool)
 import Database.Persist.Postgresql (withPostgresqlPool)
 
 import API (app)
 import Config
 
+import Model (doMigrations)
+
 main :: IO ()
 main = runNoLoggingT $ withPostgresqlPool "host=db port=5432 dbname=test user=postgres" 3
-    $ \pool -> NoLoggingT $ run 3000 (app $ Config pool)
+    $ \pool -> NoLoggingT $ do
+                    runSqlPool doMigrations pool
+                    run 3000 (app $ Config pool)
